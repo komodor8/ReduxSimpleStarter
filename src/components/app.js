4169bf2f8ce9ff93d25fd95f7e5ca5d0b0f8a6be
Farid LKHALDOUNI <flkhaldouni@safehost.com>
2021-11-02 16:24:25 +0100

    I added typescript dependencies

--- src/components/app.js
+++ src/components/app.js
@@ -11,8 +11,12 @@ export default class App extends Component {
   }
 
   render() {
+
     var message = 'Hello world';
+    let count = 5;
+    count = 'a'
     this.log(message)
+
     return (
       <div>Landing page - app.js</div>
     );
