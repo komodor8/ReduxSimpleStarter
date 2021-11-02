b4a633c41160ee3e66ec1309f7159f3b06c4d24b
Farid LKHALDOUNI <flkhaldouni@safehost.com>
2021-11-02 13:42:48 +0100

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  log(message) {
+    console.log(message);
+  }
+
   render() {
+    var message = 'Hello world';
+    this.log(message)
     return (
-      <div>React simple starter</div>
+      <div>
+        <div>Landing page - app.js</div>
+      </div>
     );
   }
 }
