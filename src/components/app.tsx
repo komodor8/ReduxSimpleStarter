65636fe6c0003385bb208dc087309253ebd54105
Farid LKHALDOUNI <flkhaldouni@safehost.com>
2021-11-03 15:42:37 +0100

    I added features in typescript

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age?: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet(person: Person) {
+    console.log("Hello " + person.name)
+  }
+
+}
+
 export default class App extends Component {
 
-  constructor(props) {
+  constructor(props: any) {
     super(props);
   }
 
-  log(message) {
+  log(message: string) {
     console.log(message);
   }
 
   render() {
-
-    var message = 'Hello world';
-    let count = 5;
-    count = 'a'
-    this.log(message)
+    let person = new Person('farid');
+    person.greet(person)
 
     return (
       <div>Landing page - app.js</div>
